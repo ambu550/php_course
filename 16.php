9c2c0f548f560244dba4ae118b6ccdb113350ca9
ambu <ambulance003@ukr.net>
2020-08-15 18:21:21 +0300

    15_08

--- 16.php
+++ 16.php
@@ -7,20 +7,26 @@ $var ='10';
 var_dump($var);
 
 $var =20;
+var_dump($var);
 $var = (string) $var;
 var_dump($var);
 
 $var =30;
+var_dump($var);
 $var = strval($var);
 var_dump($var);
+
 $var=40;
+var_dump($var);
 $var =  $var."";
 var_dump($var);
 
 $var=50;
+var_dump($var);
 $var = "{$var}";
 var_dump($var);
 
 $var=60;
+var_dump($var);
 settype($var, "string");
 var_dump($var);
--- 34.php
+++ 34.php
@@ -1,14 +1,38 @@
 <?php
 //Выведите сумму всех цифр в числе $int. Используйте цикл while или do while
 
+
+
+//вариант 1
 $int = '01123581321';
+//преобразум строку в массив
 $arr = str_split($int);
 
 $i=0;
 $rez=0;
+//пребираем масив
 do{
     $rez=$rez+$arr[$i]; $i++;
 }
 while ($i < count($arr));
 
 echo "Сумма $rez \n";
+
+//вариант 2
+
+$int = '01123581321';
+$i=1;
+$rez2=0;
+$count = strlen($int);
+//пребираем масив
+do{
+
+    $rez2 = $rez2 + substr($int, 0,1);
+
+    $int =substr($int,1);
+
+    $i++;
+}
+while ($i <= $count );
+
+echo "Сумма $rez2 \n";
