0bda1146bd36a9815072349fc3e48d0035c66863
ambu <ambulance003@ukr.net>
2020-08-27 18:03:37 +0300

    ignore

--- 34_.php
+++ 34_.php
@@ -1,9 +1,13 @@
 <?php
+
 $array = array(FALSE => TRUE, TRUE => FALSE, NULL =>
     array(PHP_OS, "PHP_VERSION"));
-$array2[NULL] = array(2 =>'Jeanne d\'Arc');
+
+$array2 = 'Jeanne d\'Arc';
+
+
 //$array3=$array2+$array;
-$array3=array_merge($array,$array2);
- print_r($array);//
-print_r($array2);//
-print_r($array3);
+$array3=array_push($array[NULL],$array2);
+// print_r($array);//
+//print_r($array2);//
+print_r($array);
