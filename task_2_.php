a984840337a579e9a705e296cc3bac924923a923
ambu <ambulance003@ukr.net>
2020-08-28 00:35:21 +0300

    done

--- task_2_.php
+++ task_2_.php
@@ -11,4 +11,33 @@ $a = array( ‘c’ => ‘apple’, ‘b’ => ‘banana’, ‘a’ => ‘cherr
 
 $a = array( 'a' => 'apple', 'b' => 'banana','c' => 'cherry');
 
+function arrayReverseKeys($a){
+    $new_a =null;
+    $cnt =  count($a);
+    $i=0;
+    $cicle=0;
+    foreach ($a as $key => $value) {
 
+
+      $temp_key[$i+1]= $key;
+      $temp_value[$i]= $value;
+        $i++;
+
+    }
+  /*  print_r($temp_key);
+    print_r($temp_value);*/
+    for ($i=0; $i<$cnt; $i++){
+
+        $new_array[$temp_key[$cnt-$i]]=$temp_value[$i] ;
+
+        $cicle++;
+    }
+    print_r($new_array)."\n";
+    echo "циклов $cicle \n";
+    return $new_array;
+
+}
+
+
+
+arrayReverseKeys($a);
