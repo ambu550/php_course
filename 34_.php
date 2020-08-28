8414f108fe01a14f25fafb889fdf754425dd7f17
ambu <ambulance003@ukr.net>
2020-08-28 17:51:23 +0300

    done

--- 34_.php
+++ 34_.php
@@ -1,13 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+
 <?php
 /*
 * 34) Назовите пары ключ\значение в массиве $array.
@@ -22,8 +13,23 @@ array(PHP_OS, "PHP_VERSION"));
 
 $array2 = 'Jeanne d\'Arc';
 array_push($array[NULL],$array2);
-
+/*
 echo ("<pre>");
-print_r($array);
 echo ("</pre>");
+*/
+
+print_r($array);
+
+var_dump($array);
+
+foreach($array as $key => $res)
+{
+    if($key|| $key===0){
+    echo "$key => $res \n";}else{
+    foreach($res  as  $in_key => $value)
+    {
+        echo "[$key][$in_key] = $value \n";
+    }}
+}
+
 ?>
