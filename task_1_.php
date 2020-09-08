756f49838c8e79bf0a0d38ff40c14e5555323db6
ambu <ambulance003@ukr.net>
2020-09-08 23:53:31 +0300

    bin_search

--- task_1_.php
+++ task_1_.php
@@ -9,11 +9,11 @@
 
 
 
-$a = [ 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ,16 ,17 ,18, 19, 20];
+$a = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15 ,16 ,17 ,18, 19, 20, 21, 22];
 
 $b = 5;
 
-$start = microtime(true);
+
 
 function serchInArray($a, $b){
     if (in_array($b, $a)) {
@@ -25,6 +25,9 @@ function serchInArray($a, $b){
     }
 
 }
+
+$start = microtime(true);
+
 $res = serchInArray($a, $b);
 
 $end = microtime(true);
@@ -37,7 +40,7 @@ var_dump(($res));
 
 ////////////вариант 2
 
-$start2 = microtime(true);
+
 global $res2;
 function serchInArray2($a, $b){
     $a=array_flip($a);
@@ -45,12 +48,56 @@ function serchInArray2($a, $b){
 
 }
 
+$start2 = microtime(true);
+
 $res2 = serchInArray2($a, $b);
 
 $end2 = microtime(true);
 $runtime2 = ($end2 - $start2)*1000;
-
 echo 'Время выполнения скрипта №2 : '. round($runtime2, 4)." миллисекунд\n";
 var_dump(($res2));
 
-////////////вариант 3
+
+////////////вариант 3 бинарный поиск
+
+
+function serchInArray3($a, $b, $start=0, $stop=null){
+
+    //определяем конец массива(или куска массива)
+    if($stop ===null){
+        //опредилили крайний элемент
+        $stop = count($a)-1;
+    }
+
+    //защита от рекурсии
+    if ($start>$stop){
+        return false;
+    }
+
+
+    //определяем середину массива
+    $middle=($start + $stop)/2;
+
+    //определяем или элемент по середине равен искомому числу
+    if ($a[$middle] !== $b){
+        //если элемент по середине меньше искомого числа то далее проверяем вторую половину массива
+        if($a[$middle] < $b){
+            $start = $middle + 1;
+            //если элемент по середине больше искомого числа то далее проверяем первую половину массива
+        }else{
+            $stop = $middle -1;
+        }
+        return  serchInArray3($a, $b, $start, $stop);
+    }
+    //если элемент по середине равен искомому числу то true
+    return true;
+
+}
+$start3 = microtime(true);
+
+$res3 =serchInArray3($a,$b);
+
+$end3 = microtime(true);
+$runtime3 = ($end3 - $start3)*1000;
+echo 'Время выполнения скрипта №3 : '. round($runtime3, 4)." миллисекунд\n";
+var_dump(($res3));
